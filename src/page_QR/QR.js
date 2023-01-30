ceb802e227cee50fdf3fb1c756f97fbfaea9b717
kusano <kusano.hatsumi@gmail.com>
2023-01-30 16:12:03 +0900

    build

--- src/page_QR/QR.js
+++ src/page_QR/QR.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./QR.scss"
 
 import { useState } from "react";
 import { useZxing } from "react-zxing";
+import useModal from './useModal';
+import ReactModal from 'react-modal';
 
 export const QR = () => {
+    // --- QRコードリーダー ---
     const [result, setResult] = useState("");
     const { ref } = useZxing({
         onResult(result) {
@@ -12,10 +15,36 @@ export const QR = () => {
         },
     });
 
+    // --- モーダル ---
+    // const { Modal, openModal, closeModal } = useModal();
+    const [show,setShow] = useState(false);
+
+
+
+
+    if (result == "") {
+        console.log("null");
+        console.log(result);
+    }else{
+        console.log("full");
+        console.log(result);
+    }
+
   return (
     <div>
         <video id='camera' ref={ref} />
         <img src={`${process.env.PUBLIC_URL}/img/${result}`} />
+        {/* <Modal>
+            <h2>Content from children</h2>
+            <video id='camera' ref={ref} />
+            <button onClick={closeModal}>Close</button>
+        </Modal> */}
+        <ReactModal 
+            isOpen={show}
+            onRequestClose={()=>setShow(false)}
+            setResult={setResult}
+        />
     </div>
+
   )
 }
---
+++ src/page_QR/useModal.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const useModal = () => {
+    const [show,setShow] = useState(false);
+
+  const openModal = ()=>{
+    setShow(true);
+  };
+  const closeModal = ()=>{
+    setShow(false);
+  };
+
+
+
+    const Modal = ({children})=>{
+        if (!show) return null; 
+        return <>{children}</>;
+    };
+  return (
+    {Modal, openModal, closeModal}
+  )
+}
+
+export default useModal;
